d890648ad45ffe937e3fa6628f922b2be6005ab1
SunspotsInys <inysuay@gmail.com>
2019-04-07 12:31:37 +0800

    按照注释尝试运行httpd.c文件

--- httpd.c
+++ httpd.c
@@ -22,7 +22,7 @@
 #include <strings.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <pthread.h>
+//#include <pthread.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 
@@ -479,7 +479,7 @@ int main(void)
  int client_sock = -1;
  struct sockaddr_in client_name;
  int client_name_len = sizeof(client_name);
- pthread_t newthread;
+//  pthread_t newthread;
 
  server_sock = startup(&port);
  printf("httpd running on port %d\n", port);
@@ -491,12 +491,13 @@ int main(void)
                        &client_name_len);
   if (client_sock == -1)
    error_die("accept");
- /* accept_request(client_sock); */
- if (pthread_create(&newthread , NULL, accept_request, client_sock) != 0)
-   perror("pthread_create");
+  accept_request(client_sock);
+//  if (pthread_create(&newthread , NULL, accept_request, client_sock) != 0)
+//    perror("pthread_create");
  }
 
  close(server_sock);
 
  return(0);
 }
+
